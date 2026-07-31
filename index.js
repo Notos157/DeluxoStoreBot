62a0d10ddcc32132dede9abde54cdb01dff726fa
Notos157 <alandamiao2003@gmail.com>
2026-07-31 12:42:27 -0300

    Atualizando codigo do bot

--- index.js
+++ index.js
@@ -408,7 +408,7 @@ client.on('messageCreate', async message => {
             // 4. Envia o aviso privado (somente para quem digitou) via DM
             try {
                 await authorTarget.send({
-                    content: `Você tomou castigo de 1 semana por cair em nossa armadilha de **bots de spam e usuários mal-intencionados**. Caso tenha recuperado sua conta, entre em contato com um ADM do servidor.`
+                    content: `Você tomou castigo de 1 semana por cair em nossa armadilha de **bots de spam e usuários mal-intencionados**. Caso tenha recuperado sua conta, entre em contato com o cargo <@&${1529320245796540436}>.`
                 });
             } catch (err) {
                 console.error('Erro ao enviar mensagem no privado (DM fechada):', err);
